fa2314f1595b132d31b2f76de812597da22c6625
Funclose <ar.doroxoff@gmail.com>
2024-10-24 09:49:00 +0300

    a

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace HW_DownoloadImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlImage = "https://cdn.pixabay.com/photo/2013/04/03/12/05/tree-99852_640.jpg";
+            string filepath = @"C:\Hello\image22.jpg";
+            string directorypath = Path.GetDirectoryName(urlImage);
+            
+                if (!Directory.Exists(directorypath))
+                {
+                    Directory.CreateDirectory(directorypath);
+                }
+            
+            
+            
+
+                WebClient client = new WebClient();
+                using (Stream stream = client.OpenRead(urlImage))
+                {
+                    using (FileStream fileStream = File.Create(filepath))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+                Console.WriteLine("File succseesful Loaded");
+            
+            //catch(Exception ex) 
+            //{
+            //    Console.WriteLine("something gone wrong");
+            //}
+        }
+    }
+}
